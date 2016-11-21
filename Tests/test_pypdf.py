f380a3a89de89d5d458f65bd57812faf8985f477
Pierre Lecuyer <pierre.lecuyer@hotmail.fr>
2016-11-21 16:35:13 +0100

    Ajout de quelques PDF de test

--- Tests/test_pypdf.py
+++ Tests/test_pypdf.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import pyPdf
+import PyPDF2
+
+wd = "C:/Users/Pierre/Documents/ENPC/3. 2A/3. S3/2. TDLOG/Projet Bag of Words/"
 
 def getPDFContent(path):
     content = ""
+    num_pages = 2
+    p = open(path, "rb")
     # Load PDF into pyPDF
-    pdf = pyPdf.PdfFileReader(file(path, "rb"))
+    pdf = PyPDF2.PdfFileReader(p)
     # Iterate pages
     for i in range(0, pdf.getNumPages()):
         # Extract text from page and add to content
@@ -14,4 +18,13 @@ def getPDFContent(path):
     #content = " ".join(content.replace(u"\xa0", " ").strip().split())
     return content
 
-print(getPDFContent("C:\Users\Pierre\Downloads\livre_malo.pdf"))
+print(getPDFContent("SCD_Uruguay_2015.pdf"))
+
+
+#liste = map(str.strip, liste)
+
+#C:\Users\Pierre\Documents\ENPC\3. 2A\3. S3\2. TDLOG\Projet Bag of Words\Données\CAS_CPS\2000
+#C:\Users\Pierre\Documents\ENPC\3. 2A\3. S3\2. TDLOG\Projet Bag of Words\Données\SCD\2016\SCD_Uzbekistan_2016.pdf
+#pdf.getNumPages()
+#CAS_Djibouti_2005.pdf
+#SCD_Uzbekistan_2016.pdf
