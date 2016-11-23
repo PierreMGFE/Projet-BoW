e83f9e1891d892dfa38796336e1ecf0c42eff4a9
Pierre Lecuyer <pierre.lecuyer@hotmail.fr>
2016-11-23 13:13:04 +0100

    Revert "Ajout de quelques PDF de test"
    This reverts commit f380a3a89de89d5d458f65bd57812faf8985f477.
    
    Revert de l'ajout des fichiers PDF

--- Tests/test_pypdf.py
+++ Tests/test_pypdf.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import PyPDF2
-
-wd = "C:/Users/Pierre/Documents/ENPC/3. 2A/3. S3/2. TDLOG/Projet Bag of Words/"
+import pyPdf
 
 def getPDFContent(path):
     content = ""
-    num_pages = 2
-    p = open(path, "rb")
     # Load PDF into pyPDF
-    pdf = PyPDF2.PdfFileReader(p)
+    pdf = pyPdf.PdfFileReader(file(path, "rb"))
     # Iterate pages
     for i in range(0, pdf.getNumPages()):
         # Extract text from page and add to content
@@ -18,13 +14,4 @@ def getPDFContent(path):
     #content = " ".join(content.replace(u"\xa0", " ").strip().split())
     return content
 
-print(getPDFContent("SCD_Uruguay_2015.pdf"))
-
-
-#liste = map(str.strip, liste)
-
-#C:\Users\Pierre\Documents\ENPC\3. 2A\3. S3\2. TDLOG\Projet Bag of Words\Données\CAS_CPS\2000
-#C:\Users\Pierre\Documents\ENPC\3. 2A\3. S3\2. TDLOG\Projet Bag of Words\Données\SCD\2016\SCD_Uzbekistan_2016.pdf
-#pdf.getNumPages()
-#CAS_Djibouti_2005.pdf
-#SCD_Uzbekistan_2016.pdf
+print(getPDFContent("C:\Users\Pierre\Downloads\livre_malo.pdf"))
