110b1fd02c904d97c785b2d2bf912c7fc7f6f35a
Pierre Lecuyer <pierre.lecuyer@hotmail.fr>
2016-11-28 18:05:39 +0100

    Modification test_PyPDF2_pierre.py

--- Conversion de PDF/test_PyPDF2_pierre.py
+++ Conversion de PDF/test_PyPDF2_pierre.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import PyPDF2
+import nltk
+#import enchant
+#d = enchant.Dict("en_US")
+
+wd = "C:/Users/Pierre/Documents/ENPC/3. 2A/3. S3/2. TDLOG/Projet Bag of Words/"
+
+#Données/CAS_CPS
+#Données/SCD
 
 def getPDFContent(path):
     content = ""
-    # Load PDF into pyPDF
-    pdf = pyPdf.PdfFileReader(file(path, "rb"))
+    # Load PDF into PyPDF2
+    
+    pdf = PyPDF2.PdfFileReader(path)
     # Iterate pages
     for i in range(0, pdf.getNumPages()):
         # Extract text from page and add to content
@@ -14,4 +23,16 @@ def getPDFContent(path):
     #content = " ".join(content.replace(u"\xa0", " ").strip().split())
     return content
 
-print(getPDFContent("C:\Users\Pierre\Downloads\livre_malo.pdf"))
+#PDF to text
+output1 = getPDFContent(wd+"Données/CAS_CPS/2000/CAS_Argentina_2000.pdf")
+#Erase all non-alphabetic characters
+output2 = ''.join([c for c in output1 if c.isalpha() or c==' '])
+#Split the text into a list of lowercase words
+output3 = [w.lower() for w in output2.split()]
+#Remove words containing less than 3 letters
+output4 = [w for w in output3 if len(w)>2]
+#Remove non-english words
+#output_set_reduced = [w for w in output_set if d.check(w)]
+
+
+print(output4)
