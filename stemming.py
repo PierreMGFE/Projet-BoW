003acc50180827ad058eecde5576d0623f5e4d65
Pierre Lecuyer <pierre.lecuyer@hotmail.fr>
2016-12-12 17:40:34 +0100

    Ajout fichier stemming.py

---
+++ stemming.py
@@ -0,0 +1,21 @@
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+
+# Il faut retirer les stopwords avant de stemmer
+
+stemmer = SnowballStemmer("english", ignore_stopwords=True)
+lemmatizer = WordNetLemmatizer()
+
+source = ["having", "have", "needs", "need", "inflation", "inflate", "developments", "developing", "aggregation",
+          "aggregated", "population", "poverty", "poor", "poorer", "men", "man", "gases", "gas", "sues", "utilized",
+          "damaged"]
+
+stems1 = [stemmer.stem(word) for word in source]
+stems2 = [lemmatizer.lemmatize(word) for word in source]
+stems3 = [stemmer.stem(word) for word in stems2]
+
+print(stems1)
+print(stems2)
+print(stems3)
+
+
