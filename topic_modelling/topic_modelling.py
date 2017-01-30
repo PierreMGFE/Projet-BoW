fdc979cd281a7cdd7a30b6ebf88b6d9f2ff7243b
Florian Mante <florian.mante@eleves.enpc.fr>
2017-01-30 14:54:12 +0100

    add files topic-modelling, cluster, distance, filenames

--- topic_modelling/topic_modelling.py
+++ topic_modelling/topic_modelling.py
@@ -10,19 +10,35 @@ import numpy as np  # a conventional alias
 import sklearn.feature_extraction.text as text
 from sklearn import decomposition
 
+#%%
+from nltk.tokenize import wordpunct_tokenize
+from nltk.stem.snowball import SnowballStemmer
+
+spec_words = [ "pdf", "imagebank", "servlet", "wdscontentserver"]
+econ_words = ["sector", "percent", "growth","figure", "country"]
+country_words = ["botswana", "lesotho", "sudan", "uganda", "mali", "côte", "ivoire", "chad", "brazil", "serbia", "haiti", "bolivia", "tunisia", "bangladesh", "panama", "colombia", "honduras", "costa", "rica", "sri", "lanka", "lebanon", "mauritius", "egypt", "uruguay", "maldives"]
+banned_words = spec_words + econ_words + country_words
+class StemTokenizer(object):
+ def __init__(self):
+     self.stemmer = SnowballStemmer("english", ignore_stopwords=False)
+ def __call__(self, doc):
+     #return [self.stemmer.stem(t) for t in wordpunct_tokenize(doc) if t.isalpha() and t.lower() not in banned_words and len(t)>1] 
+     return [t for t in wordpunct_tokenize(doc) if t.isalpha() and (t.lower() not in banned_words) and len(t)>1] 
+
+#%%
 
 
-CORPUS_PATH = os.path.join('data/texte_source/pdftotext/SCD', '2016')
+CORPUS_PATH = os.path.join('data/texte_source/pdftotext/SCD', '2015')
 
 filenames = sorted([os.path.join(CORPUS_PATH, fn) for fn in os.listdir(CORPUS_PATH)])
-
+filenames.pop(0)
 # files are located in data/texte_source/pdftotext/SCD/2016
 len(filenames)
 
 filenames[:5]
 
-vectorizer = text.CountVectorizer(input='filename', stop_words='english', min_df=12)
-
+#vectorizer = text.CountVectorizer(input='filename', stop_words='english', min_df=0.03, tokenizer = StemTokenizer())
+vectorizer = text.TfidfVectorizer(input='filename', min_df=0.03, stop_words='english', tokenizer = StemTokenizer())
 dtm = vectorizer.fit_transform(filenames).toarray()
 
 vocab = np.array(vectorizer.get_feature_names())
@@ -31,11 +47,12 @@ dtm.shape
 
 len(vocab)
 
-num_topics = 5
+num_topics = 3
 
-num_top_words = 15
+num_top_words = 30
 
 clf = decomposition.NMF(n_components=num_topics, random_state=1)
+#clf = decomposition.LatentDirichletAllocation(n_topics=num_topics, random_state=1)
 
 # this next step may take some time
 doctopic = clf.fit_transform(dtm)
@@ -81,9 +98,10 @@ for i in range(len(doctopic)):
     top_topics_str = ' '.join(str(t) for t in top_topics)
     print("{}: {}".format(novels[i], top_topics_str))
     
+#%%    
 # show the top 5 words
 for t in range(len(topic_words)):
-    print("Topic {}: {}".format(t, ' '.join(topic_words[t][:15])))
+    print("Topic {}: {}".format(t, ' '.join(topic_words[t][:30])))
     
 #austen_indices, cbronte_indices = [], []
 #
